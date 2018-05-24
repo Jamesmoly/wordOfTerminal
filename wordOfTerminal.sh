b8600eaf27a6d2b1f8998aebfd5a6b58f91d7e65
Jam Jam <james_molyneux1002@hotmail.com>
2018-05-24 10:27:11 +0100

    security update

--- wordOfTerminal.sh
+++ wordOfTerminal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-numLines=$(awk 'END{print NR}' /home/moly/Documents/Terminal/wordOfTerminal/paraulasCatala.txt)
+numLines=$(awk 'END{print NR}' ~/Documents/Terminal/wordOfTerminal/paraulasCatala.txt)
 bashLine=$(((RANDOM% numLines)+1))
-bashDef=$(awk -v var=$bashLine 'FNR == var {print $0}' /home/moly/Documents/Terminal/wordOfTerminal/paraulasCatala.txt)
+bashDef=$(awk -v var=$bashLine 'FNR == var {print $0}' ~/Documents/Terminal/wordOfTerminal/paraulasCatala.txt)
 echo $bashDef
