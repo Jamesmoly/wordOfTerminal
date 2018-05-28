d048942362b41b12916ae7defd9949ba727f7d1f
Jam Jam <james_molyneux1002@hotmail.com>
2018-05-28 10:49:08 +0100

    multiple languages

--- wordOfTerminal.sh
+++ wordOfTerminal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-numLines=$(awk 'END{print NR}' ~/Documents/Terminal/wordOfTerminal/paraulasCatala.txt)
+numLines=$(awk 'END{print NR}' ~/Documents/Terminal/wordOfTerminal/definitions.txt)
 bashLine=$(((RANDOM% numLines)+1))
-bashDef=$(awk -v var=$bashLine 'FNR == var {print $0}' ~/Documents/Terminal/wordOfTerminal/paraulasCatala.txt)
+bashDef=$(awk -v var=$bashLine 'FNR == var {print $0}' ~/Documents/Terminal/wordOfTerminal/definitions.txt)
 echo $bashDef
